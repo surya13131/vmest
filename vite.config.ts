1b26a1d350011426dccd1d899f896746fa32ccda
Suryazhains <surya@zhians.com>
2026-02-24 19:40:36 +0530

    Fix login bug and fixes good and done and fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  // Casting to any prevents the 'No overload matches this call' error
-  plugins: [react(), tailwindcss() as any],
+  plugins: [react()],
 })
